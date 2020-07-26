77015eebc2f17f1d7c871c28374d68b464ef3a20
Alexander Permyakov <pamugk@gmail.com>
2020-07-26 17:32:00 +0500

    Implemented channels exploration page; Fixed stickiness of drawer and navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 import { AccountComponent } from './pages/account/account.component';
+import { ChannelComponent } from './pages/channel/channel.component';
 import { ChannelsComponent } from './pages/channels/channels.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { SearchComponent } from './pages/search/search.component';
@@ -10,14 +11,21 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 import { profileRoutes } from './pages/profile/profile-routes.module';
 import { searchRoutes } from './pages/search/search-routes.module';
+import { AlbumComponent } from './pages/album/album.component';
+import { ArtistComponent } from './pages/artist/artist.component';
+import { PlaylistComponent } from './pages/playlist/playlist.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'account', component: AccountComponent },
+  { path: 'album/:id', component: AlbumComponent },
+  { path: 'artist/:id', component: ArtistComponent },
+  { path: 'channel/:id', component: ChannelComponent },
   { path: 'channels', component: ChannelsComponent },
+  { path: 'playlist/:id', component: PlaylistComponent },
   { path: 'profile', component: ProfileComponent, children: profileRoutes },
   { path: 'search/:query', component: SearchComponent, children: searchRoutes },
-  { path: '**', component: NotFoundComponent }
+  { path: '**', component: NotFoundComponent } 
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,31 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
+import { AccountComponent } from './pages/account/account.component';
+import { AlbumComponent } from './pages/album/album.component';
+import { ArtistComponent } from './pages/artist/artist.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DataModule } from './data/data.module';
+import { ChannelComponent } from './pages/channel/channel.component';
+import { ChannelsComponent } from './pages/channels/channels.component';
+import { HomeComponent } from './pages/home/home.component';
+import { PlaylistComponent } from './pages/playlist/playlist.component';
 import { ProfileModule } from './pages/profile/profile.module';
 import { SearchModule } from './pages/search/search.module';
-import { HomeComponent } from './pages/home/home.component';
-import { ChannelsComponent } from './pages/channels/channels.component';
+
+import { DataModule } from './data/data.module';
 
 @NgModule({
   declarations: [
     AppComponent,
+
+    AccountComponent,
+    AlbumComponent,
+    ArtistComponent,
+    ChannelComponent,
     ChannelsComponent,
-    HomeComponent
+    HomeComponent,
+    PlaylistComponent
   ],
   imports: [
     BrowserModule,
